7347a821c80b3e8b15c4b9eba0b67c162fda596f
brishketukislay <brishketu.kislay@gmail.com>
2024-09-09 01:12:25 +0100

    update operation

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const errorHandler = require('./middleware/errorHandler');
+const connectDb = require('./config/dbConnection');
 const app = express();
 // to parse PUT json 
+connectDb();
 app.use(express.json());
+app.use(errorHandler)
 
 // app.get('/',(req,res)=>{
 //     res.status(200).json({message:'home page'})
